ad36c14293869324d261770eb4a0f2327bc295b8
suicidepills <atom.mac@gmail.com>
2013-11-29 15:22:34 -0500

    Add various utility methods to utils.js.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,42 @@
+// ---------
+// Array
+// ---------
+
+// Thanks, Markus Amalthea Magnu of StackOverflow!
+// http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
+Array.prototype.random = function () {
+    return this[Math.floor(Math.random() * this.length)]
+};
+
+// ---------
+// Math
+// ---------
+
+// Thanks, numerous posts on the Internet!
 Math.randomInt = function (min, max) {
     return Math.round((max - min) * Math.random() + min);
 };
+
+// Thanks, Paul Irish: http://www.paulirish.com/2009/random-hex-color-code-snippets/
+Math.randomColor = function () {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+// ---------
+// Number
+// ---------
+
+// Thanks, numerous posts on the Internet!
 Number.prototype.clamp = function(min, max) {
     return Math.min(Math.max(this, min), max);
 };
+
+// ---------
+// Other
+// ---------
+
+// Thanks, Douglas Crockford!
+// http://javascript.crockford.com/remedial.html
 function typeOf(value) {
     var s = typeof value;
     if (s === 'object') {
