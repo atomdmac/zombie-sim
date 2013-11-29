66f6845501e7a87858bd662160fd2bb26bd0ba19
suicidepills <atom.mac@gmail.com>
2013-11-29 15:29:10 -0500

    Prevent collisions on first simulation tick().
    Previously, if a large number of Bodies were generated at the beginning
    of the simulation, collisions would occur on the first simulation iteration.
    This caused colliding Bodies to have a larger initial velocity than those
    that were not part of a collision.
    
    To fix this, we run 100 iterations of collide() prior to the first tick().  This
    seems to fix the issue but it would be nice to figure out the minimum
    number of collide() iterations that we'll need to prevent this issue.
    
    As it stands, 1 iterations is not always enough.
    
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/sim.js
+++ js/sim.js
@@ -18,8 +18,12 @@ SIM = function (scope) {
     function randomY() {
         return Math.randomInt(0, scope.config.height);
     }
-    function collide () {
-        var bodies = scope.bodies;
+    function collide (preventAccel) {
+        var bodies     = scope.bodies,
+            // Let's keep track of how many collisions happen here.
+            collisions = 0;
+            preventAccel = preventAccel || false;
+            
         for(var i=0, l=bodies.length; i<l; i++){
             var body1 = bodies[i];
             
@@ -49,11 +53,26 @@ SIM = function (scope) {
                     body1.y -= y*factor*0.5;
                     body2.x += x*factor*0.5;
                     body2.y += y*factor*0.5;
+                    
+                    collisions++;
+                    
+                    // TODO: This is an experiment.
+                    // var tmp = body1.color;
+                    body1.color = body2.color;
+                    // body2.color = tmp;
+                    
+                    // Prevent acceleration?
+                    if (preventAccel) {
+                        body1.stop();
+                        body2.stop();
+                    }
                 }
             }
         }
+        return collisions;
     }
     function tick() {
+        console.log("tick");
         scope.ctx.clearRect(0, 0,
                             scope.config.width,
                             scope.config.height);
@@ -81,23 +100,41 @@ SIM = function (scope) {
         scope.canvas = document.getElementById("stage");
         scope.width  = scope.config.width;
         scope.height = scope.config.height;
-        console.log(scope.canvas);
         scope.ctx    = scope.canvas.getContext("2d");
         
+        // Create test bodies.
         var x, y, r;
         var padding = 10;
-        for(var i=0; i<400; i++) {
+        for(var i=0; i<500; i++) {
             r = Math.randomInt(3, 5);
             x = randomX().clamp(r, scope.config.width  - r);
             y = randomY().clamp(r, scope.config.height - r);
-            console.log(r, " ", x, " ", y);
+            
+            // console.log(r, " ", x, " ", y);
+            
             scope.bodies.push(new Body(x, y, r));
         }
         
-        collide();
+       // TODO: This is messy.  Find a better hueristic to determine how many collide() iterations we need.
+        var numCollisions = 1,
+            safety = 100, safetyCount = 0;
+        while(numCollisions > 0) {
+            numCollisions = collide(true);
+            console.log("cols: ", numCollisions);
+            safetyCount++;
+            if (safetyCount > safety) {
+                console.log("breaking");
+                break;
+            }
+        };
+        console.log("itrs: ", safetyCount);
         
         // Start / Stop sim.
         scope.canvas.onclick = toggleSim;
+        // DEBUG CODE: Makes clicking thru iterations easy :)
+        /*scope.canvas.onclick = function () {
+            tick();
+        }*/
     };
     
     scope.init();
