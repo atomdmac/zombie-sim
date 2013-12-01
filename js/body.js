3226b74a885a493cc58129ec677b1ddee676178f
suicidepills <atom.mac@gmail.com>
2013-11-30 19:39:34 -0500

    Convert everything to use Resig's simple inheritence.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/body.js
+++ js/body.js
@@ -6,87 +6,89 @@
  * Sheesh, that dude is so much smarter than I am.
  */
 
-var Body = function Body (config){
-    this.initBody(config);
-};
-
-// ---------
-// Properties
-// ---------
-Body.prototype.initBody = function (config) {
-    config = config || {};
-    this.color  = config.color || "#ccc";
-    this.x      = config.x || 0;
-    this.y      = config.y || 0;
-    this.px     = this.x;
-    this.py     = this.y;
-    this.ax     = config.ax || 0;
-    this.ay     = config.ay || 0;
-    this.radius = config.radius || 5;
-    this.maxAcc = config.maxAcc || 1;
-};
-
-// ---------
-// Steering Behaviors
-// ---------
-Body.prototype.flee = function () {
-    // TODO
-}
-
-Body.prototype.seek = function () {
-    // TODO
-}
-
-Body.prototype.wander = function () {
-    var range = this.maxAcc,
+var Body = Class.extend({
+    color: null,
+    x: null,
+    y: null,
+    px: null,
+    py: null,
+    ax: null,
+    ay: null,
+    radius: null,
+    maxAcc: null,
+    
+    init: function (config) {
+        config = config || {};
+        this.color  = config.color || "#ccc";
+        this.x      = config.x || 0;
+        this.y      = config.y || 0;
+        this.px     = this.x;
+        this.py     = this.y;
+        this.ax     = config.ax || 0;
+        this.ay     = config.ay || 0;
+        this.radius = config.radius || 5;
+        this.maxAcc = config.maxAcc || 5;
+    },
+    
+    // ---------
+    // Update
+    // ---------
+    
+    draw: function (ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fill();
+    },
+    
+    tick: function () {
+        var step = 8,
+            step = 1/step;
+        
+        this.wander();
+        this.accelerate(step);
+        this.inertia(step);
+    },
+    
+    // ---------
+    // Steering Behaviors
+    // ---------
+    flee: function (target) {
+        // TODO
+    },
+    
+    seek: function (target) {
+        // TODO
+    },
+    
+    wander: function () {
+        var range = this.maxAcc,
         min   = range * -1,
         max   = range;
-    this.ax = Math.randomInt(min, max);
-    this.ay = Math.randomInt(min, max);
-}
-
-// ---------
-// Physics
-// ---------
-Body.prototype.accelerate = function (delta) {
-    this.x += this.ax * delta * delta
-    this.y += this.ay * delta * delta;
-    this.ax = 0;
-    this.ay = 0;
-}
-
-Body.prototype.stop = function () {
-    this.px = this.x;
-    this.py = this.y;
-    this.ax = 0;
-    this.ay = 0;
-}
-
-Body.prototype.inertia = function (delta) {
-    var x = this.x*2 - this.px;
-    var y = this.y*2 - this.py;
-    this.px = this.x;
-    this.py = this.y;
-    this.x = x;
-    this.y = y;
-}
-
-// ---------
-// Step / Update
-// ---------
-
-Body.prototype.draw = function (ctx) {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-    ctx.fill();
-}
-
-Body.prototype.tick = function () {
-    var step = 8,
-        step = 1/step;
+        this.ax = Math.randomInt(min, max);
+        this.ay = Math.randomInt(min, max);
+    },
     
-    this.wander();
-    this.accelerate(step);
-    this.inertia(step);
-}
+    accelerate: function (delta) {
+        this.x += this.ax * delta * delta
+        this.y += this.ay * delta * delta;
+        this.ax = 0;
+        this.ay = 0;
+    },
+    
+    inertia: function (delta) {
+        var x = this.x*2 - this.px;
+        var y = this.y*2 - this.py;
+        this.px = this.x;
+        this.py = this.y;
+        this.x = x;
+        this.y = y;
+    },
+    
+    stop: function () {
+        this.px = this.x;
+        this.py = this.y;
+        this.ax = 0;
+        this.ay = 0;
+    }
+});
--- js/human.js
+++ js/human.js
@@ -1,10 +1,75 @@
-var Human = function (config) {
-    // Initialize Body "super-class".
-    this.initBody(config);
+var Human = Body.extend({
+    stats: null,
+    status: null,
     
-    // Init Human "class".
-    this.initHuman(config);
-};
+    init: function (config) {
+        this._super(config);
+        this.stats = {
+            "defense": 10,
+            "attack" : 10
+        };
+        this.status = config.status || Human.ALIVE;
+        this.applyStatus();
+    },
+    
+    // ---------
+    // Update
+    // ---------
+    
+    tick: function () {
+        if (this.status < Human.DEAD) {
+            this._super();
+        }
+    },
+    
+    // ---------
+    // Combat
+    // ---------
+    isHostel: function (target) {
+        if (target.status == this.status) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    attack: function (target){
+        if (target.status == Human.DEAD) {
+            return
+        }
+        var attack = Math.randomFloat(0, this.stats.attack);
+        target.defend(this, attack);
+    },
+    defend: function (attacker, strength) {
+        var defense = Math.randomFloat(0, this.stats.defense);
+        if (defense < strength) {
+            this.die();
+        }
+    },
+    
+    // ---------
+    // Status
+    // ---------
+    die: function () {
+        if (this.status !== Human.DEAD) {
+            this.status++;
+        }
+        this.applyStatus();
+    },
+    
+    applyStatus: function () {
+        switch (this.status) {
+            case Human.ALIVE:
+                this.color = "blue";
+                break;
+            case Human.UNDEAD:
+                this.color = "red";
+                break;
+            case Human.DEAD:
+                this.color = "#eee";
+                break;
+        }
+    }
+});
 
 // ---------
 // Constants
@@ -12,52 +77,3 @@ var Human = function (config) {
 Human.ALIVE  = 0;
 Human.UNDEAD = 1;
 Human.DEAD   = 2;
-
-// ---------
-// Initialization
-// ---------
-Human.prototype = Body.prototype;
-Human.prototype.initHuman = function (config) {
-    // Combat stats.
-    this.stats = {
-        defense: 10,
-        attack: 10
-    }
-    
-    // Possible values:
-    // 0 = Alive
-    // 1 = Undead
-    // 2 = Dead
-    this.status = config.status || 0;
-}
-
-// ---------
-// Combat
-// ---------
-Human.prototype.attack = function (target) {
-    var attack = Math.randomFloat(0, this.stats.attack);
-    target.defend(this, attack);
-};
-
-Human.prototype.defend = function (attacker, strength) {
-    var defemse = Math.randomFloat(0, this.stats.defense);
-    if (defend < strength) {
-        this.die();
-    }
-};
-
-// ---------
-// State/Status Change
-// ---------
-Human.prototype.die = function () {
-    this.status++;
-    switch (this.status) {
-        // Undead
-        case Human.UNDEAD:
-            this.color = "#00ff00";
-            break;
-        // Dead
-        case Human.DEAD:
-            this.color = "#000";
-    }
-}
